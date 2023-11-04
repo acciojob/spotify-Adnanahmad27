6f8cad9835506335b1c75528dae425fda77bcb15
adnan <adnan232462@gmail.com>
2023-11-04 16:24:46 +0530

    first

--- src/main/java/com/driver/Album.java
+++ src/main/java/com/driver/Album.java
@@ -6,28 +6,21 @@ import java.util.List;
 public class Album {
     private String title;
     private Date releaseDate;
-
     public Album(){
-
     }
-
     public Album(String title){
         this.title = title;
         this.releaseDate = new Date();
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public Date getReleaseDate() {
         return releaseDate;
     }
-
     public void setReleaseDate(Date releaseDate) {
         this.releaseDate = releaseDate;
     }
--- src/main/java/com/driver/Playlist.java
+++ src/main/java/com/driver/Playlist.java
@@ -4,19 +4,14 @@ import java.util.List;
 
 public class Playlist {
     private String title;
-
     public Playlist(){
-
     }
-
     public Playlist(String title){
         this.title = title;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
